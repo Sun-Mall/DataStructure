fc847353065f7d261d50fb49292ca5679045d48b
penghui <penghui8411@gmail.com>
2021-02-18 20:00:35 +0800

    选择排序算法

---
+++ src/com/datastructure/ArrayGenerator.java
@@ -0,0 +1,27 @@
+package com.datastructure;
+
+import java.util.Random;
+
+/**
+ * 自动生成数组
+ *
+ * @Author Sun
+ * @Date 2021-02-18 19:56
+ */
+public class ArrayGenerator {
+
+    /**
+     * 生成一个长度为n的随机数组，数组长度的为n,数组的值为（0，bound）
+     * @param n 数组长度
+     * @param bound 随机生成的最大值
+     * @return 数组
+     */
+    public static Integer[] generateRandomArray(int n,int bound){
+        Integer[] arr = new Integer[n];
+        Random random = new Random();
+        for(int i=0;i<n;i++){
+            arr[i]=random.nextInt(bound);
+        }
+        return arr;
+    }
+}
